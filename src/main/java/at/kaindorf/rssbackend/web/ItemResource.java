c963fa3a69a6f6096f8e2ee1ae1d9384f896f88d
eibmac20 <eibmac20@htl-kaindorf.at>
2024-06-04 13:24:37 +0200

    last warning fix

--- src/main/java/at/kaindorf/rssbackend/web/ItemResource.java
+++ src/main/java/at/kaindorf/rssbackend/web/ItemResource.java
@@ -5,7 +5,6 @@ import at.kaindorf.rssbackend.pojos.ApiItemList;
 import at.kaindorf.rssbackend.pojos.RssItem;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
