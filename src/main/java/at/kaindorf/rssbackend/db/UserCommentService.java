8061ad6c7d2bb42bacb6481519a3a24209e6140c
eibmac20 <eibmac20@htl-kaindorf.at>
2024-06-13 08:46:44 +0200

    fix warns

--- src/main/java/at/kaindorf/rssbackend/db/UserCommentService.java
+++ src/main/java/at/kaindorf/rssbackend/db/UserCommentService.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- src/main/java/at/kaindorf/rssbackend/web/UserCommentResource.java
+++ src/main/java/at/kaindorf/rssbackend/web/UserCommentResource.java
@@ -9,12 +9,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
